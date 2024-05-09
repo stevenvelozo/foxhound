4da765843ea78b4efdb1d933e9ee98c3e7a85743
Steven Velozo <steven@velozo.com>
2024-05-09 08:46:40 -0700

    Updated to quackage, added sqlite

--- source/Foxhound-Dialects.js
+++ source/Foxhound-Dialects.js
@@ -2,8 +2,9 @@ getDialects = () =>
 {
 	let tmpDialects = {};
 
-	tmpDialects.ALASQL = require('./dialects/ALASQL/FoxHound-Dialect-ALASQL.js');
 	tmpDialects.English = require('./dialects/English/FoxHound-Dialect-English.js');
+	tmpDialects.SQLite = require('./dialects/SQLite/FoxHound-Dialect-SQLite.js');
+	tmpDialects.ALASQL = require('./dialects/ALASQL/FoxHound-Dialect-ALASQL.js');
 	tmpDialects.MeadowEndpoints = require('./dialects/MeadowEndpoints/FoxHound-Dialect-MeadowEndpoints.js');
 	tmpDialects.MySQL = require('./dialects/MySQL/FoxHound-Dialect-MySQL.js');
 	tmpDialects.MSSQL = require('./dialects/MicrosoftSQL/FoxHound-Dialect-MSSQL.js');
